a44b84447b4d51efc7e3d8d3a953aa29afe3bba1
Tim Ohling <trohling@peak.org>
2015-04-15 19:56:23 -0700

    adding rotational potentiometer skel

---
+++ ArdLibrary/RotationPotentiometer/RotationPotentiometer.cpp
@@ -0,0 +1,18 @@
+/*
+ * RotationPotentiometer.cpp
+ *
+ *  Created on: Apr 15, 2015
+ *      Author: trohling
+ */
+
+#include "RotationPotentiometer.h"
+
+RotationPotentiometer::RotationPotentiometer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RotationPotentiometer::~RotationPotentiometer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ArdLibrary/RotationPotentiometer/RotationPotentiometer.h
@@ -0,0 +1,17 @@
+/*
+ * RotationPotentiometer.h
+ *
+ *  Created on: Apr 15, 2015
+ *      Author: trohling
+ */
+
+#ifndef ROTATIONPOTENTIOMETER_ROTATIONPOTENTIOMETER_H_
+#define ROTATIONPOTENTIOMETER_ROTATIONPOTENTIOMETER_H_
+
+class RotationPotentiometer {
+public:
+	RotationPotentiometer();
+	virtual ~RotationPotentiometer();
+};
+
+#endif /* ROTATIONPOTENTIOMETER_ROTATIONPOTENTIOMETER_H_ */
